a2ea6378d14c1fb1c69dc53061883d33fa704304
PabloR9080 <a19216279@alumnos.uady.mx>
2022-04-13 17:50:41 -0500

    Schemas updated

--- Schemas.py
+++ Schemas.py
@@ -1,16 +1,8 @@
-#Schema utiliza un "Meta" que mapea los campos de la base de datos
-# a un objeto python serializado.
 
 from Models import User
 from config import ma 
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-#class UserSchema(ma.Schema):
-#    class Meta:
-#        fields = ('id','name','lastname')
-
-#Schema que genera automaticamente a partir de un modelo o tabla de SQLAlchemy
-# el modelo se asigna mediante la opción model en la clase Meta
 class UserSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = User
